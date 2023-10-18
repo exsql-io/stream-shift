2c1bb38deb4e4564adee9d04ce792beccbbeab0e
Benjamin Schaff <benjamin.schaff@gmail.com>
2023-10-17 20:57:08 -0400

    feature: basic kafka cli  - partial support for tailing kafka topic.

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -2,14 +2,13 @@ use std::error::Error;
 use std::time::Duration;
 
 use clap::{Parser, Subcommand};
-use futures::StreamExt;
 use rdkafka::admin::AdminClient;
-use rdkafka::config::FromClientConfig;
-use rdkafka::consumer::{Consumer, DefaultConsumerContext, StreamConsumer};
-use rdkafka::util::DefaultRuntime;
-use rdkafka::{ClientConfig, Message, TopicPartitionList};
+use rdkafka::client::DefaultClientContext;
+use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::{ClientConfig, Message};
 use tabled::settings::{Settings, Style};
 use tabled::{Table, Tabled};
+use uuid::Uuid;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -34,8 +33,11 @@ enum Commands {
 enum TopicCommandActions {
     List {},
     Tail {
+        #[arg(long, value_name = "name")]
         name: String,
+        #[arg(short, value_name = "n")]
         n: Option<usize>,
+        #[arg(short, long, value_name = "follow")]
         follow: Option<bool>,
     },
 }
@@ -43,9 +45,15 @@ enum TopicCommandActions {
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(30);
 
 #[derive(Tabled)]
-struct Topic {
+struct TabledTopic {
     name: String,
     partitions: usize,
+}
+
+#[derive(Tabled)]
+struct TabledMessage {
+    key: String,
+    value: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -54,50 +62,57 @@ fn main() -> Result<(), Box<dyn Error>> {
         Commands::Topic { action } => match action {
             TopicCommandActions::List {} => {
                 let mut config = ClientConfig::new();
-                config.set("bootstrap.servers", cli.bootstrap_address);
+                let admin: AdminClient<DefaultClientContext> = config
+                    .set("bootstrap.servers", cli.bootstrap_address)
+                    .set("client.id", "stream-shift-cli")
+                    .create()?;
 
-                let admin = AdminClient::from_config(&config)?;
                 let client = admin.inner();
                 let describe = client.fetch_metadata(None, DEFAULT_TIMEOUT)?;
 
                 let mut topics = Vec::new();
                 for topic in describe.topics() {
-                    topics.push(Topic {
+                    topics.push(TabledTopic {
                         name: topic.name().to_string(),
                         partitions: topic.partitions().len(),
                     });
                 }
 
                 let table_config = Settings::default().with(Style::psql());
-                println!("Topics: \n{}", Table::new(topics).with(table_config));
+                println!("{}", Table::new(topics).with(table_config));
 
                 drop(admin)
             }
             TopicCommandActions::Tail { name, n, follow } => {
                 let mut config = ClientConfig::new();
-                config.set("bootstrap.servers", cli.bootstrap_address);
+                let consumer: BaseConsumer = config
+                    .set("bootstrap.servers", cli.bootstrap_address)
+                    .set("group.id", format!("stream-shift-cli-{}", Uuid::new_v4()))
+                    .set("enable.auto.commit", "false")
+                    .set("auto.offset.reset", "earliest")
+                    .create()?;
 
-                let consumer =
-                    StreamConsumer::<DefaultConsumerContext, DefaultRuntime>::from_config(&config)?;
+                let n = n.unwrap_or(10);
+                let _follow = follow.unwrap_or(false);
 
-                let mut topics = TopicPartitionList::new();
-                topics.add_topic_unassigned(name);
+                consumer.subscribe(&[name])?;
 
-                consumer.assign(&topics)?;
+                let mut messages = Vec::new();
+                for message in consumer.iter().take(n) {
+                    let message = message.unwrap();
+                    let key: &str = message.key_view().unwrap().unwrap();
+                    let value: &[u8] = message.payload().unwrap();
 
-                let result = async {
-                    let messages = consumer.stream().collect::<Vec<_>>().await;
-                    for message in messages {
-                        let message = message.unwrap();
-                        println!("key: {:?}, value: {:?}", message.key(), message.payload())
-                    }
-                };
+                    messages.push(TabledMessage {
+                        key: key.to_string(),
+                        value: String::from_utf8_lossy(value).to_string(),
+                    });
+                }
 
-                tokio::runtime::Builder::new_current_thread()
-                    .enable_all()
-                    .build()
-                    .unwrap()
-                    .block_on(result)
+                let table_config = Settings::default().with(Style::psql());
+                println!("{}\n", Table::new(messages).with(table_config));
+
+                drop(consumer)
             }
         },
     }
